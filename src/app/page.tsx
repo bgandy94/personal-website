732b5b5e749f859f46bdbac4e1e89d08334fedbd
Brandon Gandy <brandongundy@gmail.com>
2024-04-01 22:34:25 -0500

    fix family pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,25 @@ const interests = [
   'Home Renovation/Repair',
   'Carpentry',
   'Fishing',
+  'Anything at the lake',
+  'Music',
+  'Video games',
   'Golf',
 ]
 export default function Home() {
   return (
     <>
+    <div className='max-md:max-w-[50%]'>
       <Image
         src="/fam-pic.svg"
         alt="family picture"
+        layout='response'
         className="rounded-[50rem]"
         width={400}
         height={400}
       />
+
+    </div>
       <h1 className="text-3xl mb-4">Hi, I&apos;m Brandon.</h1>
       <p>
         As you can see in the picture above, first and foremost I&apos;m a
@@ -32,8 +39,8 @@ export default function Home() {
         </Link>{' '}
         to see what I&apos;ve been up to! As a family, we&apos;ve amassed a
         small portfolio of long-term rental properties here in Oklahoma. Given
-        we self-manage our rentals, I still tend to attempt to perform as much
-        of the maintenance and repairs I can with my own mental health in mind.
+        we self-manage our rentals, I still attempt to perform as much
+        of the maintenance and repairs with my own mental health in mind.
       </p>
 
       <h1 className="text-3xl mt-8 mb-4">Interests</h1>

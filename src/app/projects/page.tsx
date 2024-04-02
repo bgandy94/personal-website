558d327cc0e96ffb7978581eda54dd9a2e968dc8
Brandon Gandy <brandongandy2012@gmail.com>
2024-04-01 21:16:49 -0500

    skip steps depending on changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import { projects } from '@/src/lib/projects'
 import Link from 'next/link'
 import React from 'react'
 
-export default function Projects({ params }: { params: { slug: string } }) {
+export default function Projects() {
   return (
     <>
       <h1 className="text-3xl mb-4">Projects</h1>

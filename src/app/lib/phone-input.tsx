32b356d383ddb385a9a6d90d2c361c18062b0b94
Brandon Gandy <brandongundy@gmail.com>
2024-12-03 10:52:41 -0600

    new text page

---
+++ src/app/lib/phone-input.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+interface PhoneInputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  value?: string
+  onChange?: (value: string) => void
+}
+const formatPhoneNumber = (value: string): string => {
+  const cleaned = value.replace(/[^0-9]/g, '')
+  const match = cleaned.match(/^([0-9]{0,3})([0-9]{0,3})([0-9]{0,4})$/)
+
+  if (!match) return value
+
+  const [, areaCode, centralOfficeCode, lineNumber] = match
+  if (lineNumber) {
+    return `(${areaCode})${centralOfficeCode}-${lineNumber}`
+  } else if (centralOfficeCode) {
+    return `(${areaCode})${centralOfficeCode}`
+  } else if (areaCode) {
+    return `(${areaCode}`
+  }
+  return ''
+}
+
+const PhoneInput: React.FC<PhoneInputProps> = ({
+  value = '',
+  onChange,
+  ...rest
+}) => {
+  const [inputValue, setInputValue] = useState<string>(formatPhoneNumber(value))
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const rawValue = event.target.value.replace(/[^0-9]/g, '')
+    const formattedValue = formatPhoneNumber(rawValue)
+
+    setInputValue(formattedValue)
+    if (onChange) {
+      onChange(rawValue)
+    }
+  }
+
+  return (
+    <input
+      {...rest}
+      value={inputValue}
+      onChange={handleInputChange}
+      maxLength={13}
+    />
+  )
+}
+
+export default PhoneInput
---
+++ src/app/text/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { useState } from 'react'
+import PhoneInput from '../lib/phone-input'
+const PhonePage = () => {
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  return (
+    <div className="flex-col flex">
+      <label htmlFor="number">
+        Number to text:
+        <PhoneInput
+          className="ml-2 text-black"
+          type="tel"
+          value={phoneNumber}
+          onChange={setPhoneNumber}
+        />
+      </label>
+      {phoneNumber.length === 10 && (
+        <a
+          className="bg-primary mt-2 p-2 w-1/3"
+          href={encodeURI(
+            `mailto:mail@anomaly-email.gandy.me?subject=+1${phoneNumber} Text Thread`
+          )}
+        >
+          Mailto Link
+        </a>
+      )}
+    </div>
+  )
+}
+
+export default PhonePage

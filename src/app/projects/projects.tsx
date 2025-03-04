f2a8eb8ca010d58b8fbf6a15185356fc9c4d7f09
Brandon Gandy <brandongandy2012@gmail.com>
2025-03-03 23:22:19 -0600

    start on new report article

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { booksAutomationProject } from './books-automation.project'
 import { customBusinessPhoneProject } from './custom-business-phone.project'
 import { atbSystemProject } from './atb-system.project'
+import { portfolioReportingProject } from './portfolio-reporting.project'
 
 export type Project = {
   slug: string
@@ -15,6 +16,7 @@ export type Project = {
 }
 
 export const projects: Project[] = [
+  portfolioReportingProject,
   booksAutomationProject,
   customBusinessPhoneProject,
   atbSystemProject,
